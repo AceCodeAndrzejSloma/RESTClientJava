6630d875a05a581ce39cb3af43720c602eb9c230
AceCodeAndrzejSloma <acecode.andrzej.sloma@gmail.com>
2018-06-29 00:07:42 +0200

    dummy change

--- src/pl/acecode/Main.java
+++ src/pl/acecode/Main.java
@@ -3,7 +3,7 @@ package pl.acecode;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println(System.getProperty("java.version"));
+		System.out.println("dummy");
 	}
 
 }
